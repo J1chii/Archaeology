1e906ef6c079caaba67734c4eff1933779594853
J1chii <jc1mej9@gmail.com>
2026-05-20 21:34:55 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
 function quote() {
   document.getElementById("quoteText").innerHTML =
-  '"Ang hindi marunong lumingon sa pinanggalingan..." – Jose Rizal';
+    '"Ang hindi marunong lumingon sa pinanggalingan..." – Jose Rizal';
 }
